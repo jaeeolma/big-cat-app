5398d64ab8c87d3bf5c2d7c5e7e7a5745cfdc62e
Janne Mäyrä <jaeeolma@student.jyu.fi>
2019-02-21 09:58:08 +0200

    hekkin dam

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web: gunicorn app/server.py serve
+web: gunicorn app/server:server
--- app/server.py
+++ app/server.py
@@ -95,5 +95,5 @@ async def get_bytes(url):
             return await response.read()
 
 if __name__ == '__main__':
-    if 'serve' in sys.argv: uvicorn.run(app, host='0.0.0.0', port=8080)
+    uvicorn.run(app, host='0.0.0.0', port=8080)
 
