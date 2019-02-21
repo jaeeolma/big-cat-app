44e37543f96823e080db291867e9441c2d445e39
Janne Mäyrä <jaeeolma@student.jyu.fi>
2019-02-21 10:38:32 +0200

    download on startup

--- Procfile
+++ Procfile
@@ -1,1 +1,2 @@
-web: gunicorn app.server:app
+worker: wget https://www.dropbox.com/s/dhibuphohfwe28w/big_cats_fastai_stage2.pth?raw=1 -P app/models/
+web: gunicorn app.server:start_app
--- app/server.py
+++ app/server.py
@@ -94,7 +94,7 @@ async def get_bytes(url):
         async with session.get(url) as response:
             return await response.read()
 
-def app():
+def start_app():
     uvicorn.run(app, host='0.0.0.0', port=8080)
 
 if __name__ == '__main__':
