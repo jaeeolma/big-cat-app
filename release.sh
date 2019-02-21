5e5b5d375eedc7247eb97b612bbcbaed8d2ef758
Janne Mäyrä <jaeeolma@student.jyu.fi>
2019-02-21 11:31:33 +0200

    fix hopefully

--- release.sh
+++ release.sh
@@ -1,6 +1,6 @@
 pwd
 ls
-wget -O big_cats_fastai_stage2.pth https://www.dropbox.com/s/dhibuphohfwe28w/big_cats_fastai_stage2.pth?raw=1 -P app/models/ -nv
 cd app/models
+wget -O big_cats_fastai_stage2.pth https://www.dropbox.com/s/dhibuphohfwe28w/big_cats_fastai_stage2.pth?raw=1 -nv
 ls
 cd -
